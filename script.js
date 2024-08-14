c373287a9a509528fe5addedf2124a12752d72bb
giteshpandit <panditgitesh38@gmail.com>
2024-08-14 16:57:02 +0530

    created a submission

--- script.js
+++ script.js
@@ -13,11 +13,11 @@ function secondHighest(arr) {
     
     // Return the second highest element
     return sortedUniqueElements[1];
-	console.log(secondHighest([5, 1, 2, 3, 4])); // Output: 4
+	}
+console.log(secondHighest([5, 1, 2, 3, 4])); // Output: 4
 console.log(secondHighest([-1, -2, -3, -4, -5])); // Output: -2
 console.log(secondHighest([])); // Output: -Infinity
 console.log(secondHighest([1])); // Output: -Infinity
 console.log(secondHighest([1, 1, 1, 1, 1])); // Output: -Infinity
 
-}
 
